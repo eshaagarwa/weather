8d8da367b016632c10f1e7b49fd70fe7308d50b0
esha995 <eshaaaagarwal996@gmail.com>
2023-05-29 00:29:15 +0530

    commit

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "16ac61a07843819a2d6ee0426af0f70c",
   base: "https://api.openweathermap.org/data/2.5/",
 };
